6d1e6cd931c5cb1f83ff0772aef72869993ea47d
Eduard Tikhomirov <tikhomirovcloud@icloud.com>
2020-12-03 18:10:44 +0300

    Task1 done

--- src/tasks/Task1.java
+++ src/tasks/Task1.java
@@ -5,6 +5,7 @@ import common.PersonService;
 import common.Task;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -21,7 +22,10 @@ public class Task1 implements Task {
   // !!! Редактируйте этот метод !!!
   private List<Person> findOrderedPersons(List<Integer> personIds) {
     Set<Person> persons = PersonService.findPersons(personIds);
-    return Collections.emptyList();
+
+    return persons.stream()
+            .sorted(Comparator.comparingInt(Person::getId))
+            .collect(Collectors.toList());
   }
 
   @Override
